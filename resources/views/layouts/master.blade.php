3bd89d9fc365171ed6d4a84dba0a90f286e76d8f
japarsons <johnparsons932@gmail.com>
2019-05-22 13:13:38 +0100

    Added a master blade template and welcome blade

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title')</title> <?php //hook for getting the title ?>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')  <?php // hook for getting the body content ?>
+        </div>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title') <?php //define the title content to be inserted in to @yield('title') ?>
+    Welcome!
+@endsection
+
+@section('content') <?php //define the body content to be inserted in to @yield('content') ?>
+    <p>This is my body content.</p>
+@endsection
