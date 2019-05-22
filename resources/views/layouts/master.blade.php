a03cb32887636034985788e737c27d557fb37d6e
JAParsons <johnparsons932@gmail.com>
2019-05-22 14:37:02 +0100

    header blade added and included in master blade, bootstrap welcome page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html>
+    
     <head>
         <title>@yield('title')</title> <?php //hook for getting the title ?>
+        @include('includes.header') <?php //include the navbar header ?>
     </head>
+    
     <body>
         <div class="container">
-            @yield('content')  <?php // hook for getting the body content ?>
+            @yield('content')  <?php //hook for getting the body content ?>
         </div>
     </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,44 @@
 @endsection
 
 @section('content') <?php //define the body content to be inserted in to @yield('content') ?>
-    <p>This is my body content.</p>
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Register</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input class="form-control" type="text" name="email" id="email"></input>
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your First Name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name"></input>
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password"></input>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+
+        <div class="col-md-6">
+            <h3>Login</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input class="form-control" type="text" name="email" id="email"></input>
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your First Name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name"></input>
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password"></input>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        
+    </div>
 @endsection
